913512c659f4c14de02e87feffa59e6958abb59e
TallesSilva <tallesbugs@hotmail.com>
2019-11-28 14:49:28 -0300

    makemigrations and migrate

--- secretary/secretary/settings.py
+++ secretary/secretary/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'secretary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
