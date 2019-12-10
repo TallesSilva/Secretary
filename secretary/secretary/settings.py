58db20205c250aa32890c3831429afd670b7de11
TallesSilva <tallesbugs@hotmail.com>
2019-12-10 14:03:27 -0300

    modificação

--- secretary/secretary/settings.py
+++ secretary/secretary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*cg1n3@_!_wy-z#n%kd6h1vwd7ivx^_bcy_ps4fosoad6!rbs)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["18.231.110.160", '192.168.1.22', '192.168.0.190', '192.168.99.100', 'localhost']
+ALLOWED_HOSTS = ["52.67.163.89", '192.168.1.22', '192.168.0.190', '192.168.99.100', 'localhost']
 
 
 # Application definition
